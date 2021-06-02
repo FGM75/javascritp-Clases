186a3ea597aa66e94772303fc7410ad1d1622290
Jose Maria Zamora Perez <jzamora@biada.net>
2021-06-02 17:32:22 +0200

    creacion luchador, rey,escudero.etc

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ class Personajes {
   familia;
   edad;
   vivo = true;
+  rey = false;
+  luchador = false;
+  asesor = false;
+  escudero = false;
   constructor(nombre, familia, edad, vivo) {
     this.nombre = nombre;
     this.familia = familia;
@@ -15,35 +19,48 @@ class Personajes {
   morir() {
     this.vivo = false;
   }
-};
+}
 
-// const joffrey = new Personajes("Joffrey Baratheon", "Baratheon", 17, true);
-// joffrey.morir();
-// console.log(joffrey);
+const joffrey = new Personajes("Joffrey Baratheon", "Baratheon", 17);
+joffrey.morir();
+
+console.log(Math.floor(Math.random() * 11));
 
 class Rol extends Personajes {
-  super(nombre);
   rey = false;
   luchador = false;
   asesor = false;
   escudero = false;
-  rey(){
-    this.rey = true;
-  };
-   luchador(){
-    this.luchador = true;
-  };
-   asesor(){
-    this.asesor = true;
-  };
-   escudero(){
-    this.escudero = true;
-  };
-asd   
-  constructor(rey,luchador,asesor,escudero){
+  constructor(nombre, rey, luchador, asesor, escudero) {
+    super(nombre);
     this.asesor = asesor;
     this.luchador = luchador;
     this.rey = rey;
-    this.escudero=escudero;
+    this.escudero = escudero;
+  }
+  comunicar(mensaje) {
+    return `${mensaje}`;
+  }
+  arma() {
+    return Math.floor(Math.random() * 11) + "destreza arma";
+  }
+  rey() {
+    this.rey = true;
+    return this.comunicar("Vais a morir todos");
+  }
+  luchador() {
+    this.luchador = true;
+    const armaLuchador = arma();
+    return this.comunicar("Primero pego y luego pregunto") + armaLuchador;
+  }
+  asesor() {
+    this.asesor = true;
+    return this.comunicar("No sé por qué, pero creo que voy a morir pronto");
+  }
+  escudero() {
+    this.escudero = true;
+    return this.comunicar("Soy un loser");
   }
 }
+joffrey = new Rol().luchador();
+console.log(joffrey);
