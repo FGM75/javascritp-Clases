f2b3c21a57bedf705437d750c29fa3fd6897febe
Jose Maria Zamora Perez <jzamora@biada.net>
2021-06-02 17:02:52 +0200

    creacion clase de roles

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ class Personajes {
   nombre;
   familia;
   edad;
-  vivo;
+  vivo = true;
   constructor(nombre, familia, edad, vivo) {
     this.nombre = nombre;
     this.familia = familia;
@@ -15,18 +15,35 @@ class Personajes {
   morir() {
     this.vivo = false;
   }
-}
-
+};
 
 // const joffrey = new Personajes("Joffrey Baratheon", "Baratheon", 17, true);
 // joffrey.morir();
 // console.log(joffrey);
 
 class Rol extends Personajes {
-  rey;
-  luchador;
-  asesor;
-  escudero;
-  
-
+  super(nombre);
+  rey = false;
+  luchador = false;
+  asesor = false;
+  escudero = false;
+  rey(){
+    this.rey = true;
+  };
+   luchador(){
+    this.luchador = true;
+  };
+   asesor(){
+    this.asesor = true;
+  };
+   escudero(){
+    this.escudero = true;
+  };
+asd   
+  constructor(rey,luchador,asesor,escudero){
+    this.asesor = asesor;
+    this.luchador = luchador;
+    this.rey = rey;
+    this.escudero=escudero;
+  }
 }
