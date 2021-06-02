43ed24c1c3722fb48d3459422e0c95b0dc85e62a
Jose Maria Zamora Perez <jzamora@biada.net>
2021-06-02 20:38:09 +0200

    limpieza de codigo de pruebas anteriores y cambio de class de letra mayuscula(escudero) a minuscula y constante (Jaime) de mayuscula a minuscula

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ class Personajes {
     this.familia = familia;
     this.edad = edad;
   }
-
   //importante
   morir() {
     this.vivo = false;
@@ -24,49 +23,8 @@ class Luchador extends Personajes {
     this.arma = arma;
   }
 }
-const Jaime = new Luchador("Jaime", "Lannister", 40, "espadon");
 
-console.log(Jaime);
-
-/* class Acciones extends Personajes {
-
-  rey = false;
-  luchador = false;
-  asesor = false;
-  escudero = false;
-  mensaje2 = {
-    mensajeRey: "Primero pego y luego pregunto",
-    mensajeEscudero: "cosa",
-    mens,
-  };
-  comunicar(mensaje) {
-    console.log(`${this.mensaje}`);
-  }
-  constructor(nombre, familia, edad) {
-    super(nombre, familia, edad);
-  }
-  arma() {
-    return Math.floor(Math.random() * 11) + "destreza arma";
-  }
-  hacerRey() {
-    this.rey = true;
-    this.mensaje2.mensajeRey;
-  }
-
-  luchador() {
-    this.luchador = true;
-    const armaLuchador = arma();
-    return this.comunicar("Primero pego y luego pregunto");
-  }
-  asesor() {
-    this.asesor = true;
-    return this.comunicar("No sé por qué, pero creo que voy a morir pronto");
-  }
-  escudero() {
-    this.escudero = true;
-    //return this.comunicar("Soy un loser");
-  }
-} */
+const jaime = new Luchador("Jaime", "Lannister", 40, "espadon");
 
 class Rey extends Personajes {
   añosDeReinado;
@@ -79,47 +37,22 @@ class Rey extends Personajes {
     console.log(this.mensajeRey);
   }
 }
+
 const joffrey = new Rey("Joffrey", "Lannister", 17, 3);
 joffrey.hablar();
 console.log(joffrey);
 
-const daenerys = new Rol(
-  "Daenerys",
-  "Targaryen",
-  25,
-  "primero pego luego pregunto"
-);
-daenerys.comunicar();
-console.log(daenerys);
-
-const tyrion = new Rol("Tyrion", "Lannister", 89, "holita");
-daenerys.comunicar();
-console.log(daenerys);
-
-class escudero extends Personajes {
+class Escudero extends Personajes {
   nivelpelotismo;
   mensajeEscudero;
   personajeAlQueSirve;
+  mensajeEscudero = "Soy un Loser L";
+  nivelpelotismo = Math.floor(Math.random() * 11);
+  personajeAlQueSirve = jaime.name;
 
-  constructor(
-    nombre,
-    familia,
-    edad,
-    nivelpelotismo,
-    mensajeEscudero,
-    personajeAlQueSirve
-  ) {
+  constructor(nombre, familia, edad) {
     super(nombre, familia, edad);
-    this.nivelpelotismo = nivelpelotismo;
-    this.mensajeEscudero = "Soy un Loser L";
-    this.personajeAlQueSirve = jaime;
   }
 }
-const bronn = new escudero(
-  "Bronn",
-  "NuNAFamily",
-  78,
-  6,
-  mensajeEscudero,
-  "jaime"
-);
+const bronn = new Escudero("Bronn", "NuNAFamily", 78);
+console.log(Personajes);
